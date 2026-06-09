e118453b65f8e046fb23170cbea6d50bb5ff242f
Mateusz Skrzątek <skrzatekmateusz@gmail.com>
2026-06-09 18:35:00 +0200

    delete unused import, minor fixes

--- src/eda/movies/__init__.py
+++ src/eda/movies/__init__.py
@@ -1,9 +1,9 @@
 from .checks import (
-    movies_identifier_report,
-    movies_missingness_report,
-    movies_text_quality_report,
-    movies_year_report,
-    rotten_tomatoes_coverage_report,
+                     movies_identifier_report,
+                     movies_missingness_report,
+                     movies_text_quality_report,
+                     movies_year_report,
+                     rotten_tomatoes_coverage_report,
 )
 
 __all__ = [
